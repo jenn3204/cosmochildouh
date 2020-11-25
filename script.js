110434b1a20f116abd23a9880e2e338f03908747
jenniferjaque <jenn3204@stud.kea.dk>
2020-11-25 13:09:33 +0100

    første, rå udgave af form på forsiden der henter brugernavne fra json fil

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict"; 
+
+showLoader(); 
+window.addEventListener("DOMContentLoaded", start); 
+
+let brugere = []; 
+
+function showLoader() {
+
+    let pageState = document.readyState; 
+if (pageState == "loading") {
+    console.log("HEJ"); 
+} else {
+    console.log("NEJ"); 
+}
+}
+
+function start() {
+    showLoader(); 
+    loadBrugere(); 
+
+    document.querySelector("#logind_knap").addEventListener("click", tjekBrugere);
+}
+
+async function loadBrugere() {
+    const response = await fetch("brugere.json");
+    let jsonData  = await response.json();
+
+    brugere = jsonData; 
+  console.log(brugere); 
+  tjekBrugere(); 
+}
+
+function tjekBrugere() {
+    let brugerValue = document.querySelector("#brugernavn_input").value; 
+    let kodeValue = document.querySelector("#kodeord_input").value;
+    console.log(brugerValue, kodeValue); 
+    
+    brugere.forEach(bruger => {
+        if (bruger.brugernavn == brugerValue && bruger.kodeord == kodeValue) {
+            console.log("du er logget ind"); 
+        } else {
+            console.log("du er ikke logget ind"); 
+        }
+    })
+}
